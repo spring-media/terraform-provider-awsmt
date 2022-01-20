1547f3b3d884bd53428e480cc2bed75b201a59dc
thatsddr <domenico.diruocco@code.berlin>
2022-01-20 12:10:55 +0100

    chore: add templates for plugin and main file

--- mediatailor/provider.go
+++ mediatailor/provider.go
@@ -1,1 +1,10 @@
 package mediatailor
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap:   map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{},
+	}
+}
