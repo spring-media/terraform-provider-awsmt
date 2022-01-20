09a6e69038852c5dedd7e2578271a38f8b4f93db
thatsddr <domenico.diruocco@code.berlin>
2022-01-20 12:17:13 +0100

    chore: add configuration data source template

--- mediatailor/data_source_configuration.go
+++ mediatailor/data_source_configuration.go
@@ -1,1 +1,18 @@
 package mediatailor
+
+import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceConfiguration() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceConfigurationRead,
+		Schema:      map[string]*schema.Schema{},
+	}
+}
+func dataSourceConfigurationRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
--- mediatailor/provider.go
+++ mediatailor/provider.go
@@ -5,6 +5,6 @@ import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		ResourcesMap:   map[string]*schema.Resource{},
-		DataSourcesMap: map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{"mediatailor_configuration": dataSourceConfiguration()},
 	}
 }
