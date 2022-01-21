fd6ad0f58229c0dddeb03436b8016d3e27a576b9
thatsddr <domenico.diruocco@code.berlin>
2022-01-21 17:16:52 +0100

    test: start implementing data source tests

---
+++ mediatailor/data_source_configuration_test.go
@@ -0,0 +1,55 @@
+package mediatailor
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"os"
+	"testing"
+)
+
+func TestAccConfigurationDataSource_basic(t *testing.T) {
+	//dataSourceName := "data.mediatailor_configuration.c1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfigurationDataSource_basic(),
+				Check:  resource.ComposeTestCheckFunc(
+				//tests go here
+				),
+			},
+		},
+	})
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("AWS_ACCESS_KEY_ID"); v == "" {
+		t.Fatal("AWS_ACCESS_KEY_ID must be set for acceptance tests")
+	}
+	if v := os.Getenv("AWS_SECRET_ACCESS_KEY"); v == "" {
+		t.Fatal("AWS_SECRET_ACCESS_KEY must be set for acceptance tests")
+	}
+}
+
+func testAccConfigurationDataSource_basic() string {
+	return fmt.Sprintf(`
+terraform {
+  required_providers {
+    mediatailor = {
+      version = "0.1"
+      source  = "test/ott/mediatailor"
+    }
+  }
+}
+
+data "mediatailor_configuration" "c1" {
+  //name = "staging-live-stream"
+}
+
+output "out" {
+  value = data.mediatailor_configuration.c1
+}
+`)
+}
