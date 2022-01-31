cb7e754c2b89ed5958bc0d3175b1f2164f20e6de
thatsddr <domenico.diruocco@code.berlin>
2022-01-31 10:02:41 +0100

    chore: fetch provider from tf registry in the example

--- examples/main.tf
+++ examples/main.tf
@@ -1,16 +1,19 @@
 terraform {
   required_providers {
-    mediatailor = {
+    awsmt = {
       version = "1.0.6"
-      source  = "github.com/spring-media/ott-tfprovider-awsmt"
+      source  = "spring-media/awsmt"
+      // to use a local version of the provider,
+      // run `make` and set the source to:
+      // source = "github.com/spring-media/terraform-provider-awsmt"
     }
   }
 }
 
-data "mediatailor_configuration" "c1" {
+data "awsmt_configuration" "c1" {
   name = "staging-live-stream"
 }
 
 output "out" {
-  value = data.mediatailor_configuration.c1
+  value = data.awsmt_configuration.c1
 }
