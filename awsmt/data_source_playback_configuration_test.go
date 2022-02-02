d17d948ced034e2acf3f6303bac5593996609877
thatsddr <domenico.diruocco@code.berlin>
2022-02-02 15:10:17 +0100

    test: check error if name is not provided

--- awsmt/data_source_playback_configuration_test.go
+++ awsmt/data_source_playback_configuration_test.go
@@ -3,6 +3,7 @@ package awsmt
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"os"
+	"regexp"
 	"testing"
 )
 
@@ -13,10 +14,27 @@ func TestAccPlaybackConfigurationDataSourceBasic(t *testing.T) {
 		ProviderFactories: ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPlaybackConfigurationDataSourceBasic(),
+				Config: testAccPlaybackConfigurationDataSource1(),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.awsmt_playback_configuration.c1", "name", "broadcast-prod-live-stream"),
 				),
+			},
+		},
+	})
+}
+
+func TestAccPlaybackConfigurationDataSourceError(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPlaybackConfigurationDataSource2(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.awsmt_playback_configuration.c2", "name", ""),
+				),
+				ExpectError: regexp.MustCompile("`name` parameter required"),
 			},
 		},
 	})
@@ -28,14 +46,23 @@ func testAccPreCheck(t *testing.T) {
 	}
 }
 
-func testAccPlaybackConfigurationDataSourceBasic() string {
+func testAccPlaybackConfigurationDataSource1() string {
 	return `
 data "awsmt_playback_configuration" "c1" {
   name = "broadcast-prod-live-stream"
 }
-
 output "out1" {
   value = data.awsmt_playback_configuration.c1
+}
+`
+}
+func testAccPlaybackConfigurationDataSource2() string {
+	return `
+data "awsmt_playback_configuration" "c2" {
+  name = ""
+}
+output "out2" {
+  value = data.awsmt_playback_configuration.c2
 }
 `
 }
