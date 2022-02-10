e9de34cfaf23a3bb1de1cd82defb5fc8d3dd0178
thatsddr <domenico.diruocco@code.berlin>
2022-02-10 12:43:15 +0100

    chore: update example

--- examples/main.tf
+++ examples/main.tf
@@ -10,10 +10,27 @@ terraform {
   }
 }
 
-data "awsmt_playback_configuration" "c1" {
-  name="broadcast-staging-live-stream"
+#data "awsmt_playback_configuration" "c1" {
+#  name="broadcast-staging-live-stream"
+#}
+
+resource "awsmt_playback_configuration" "r1" {
+  ad_decision_server_url = "https://exampleurl.com/"
+  cdn_configuration {
+    ad_segment_url_prefix = "test"
+    content_segment_url_prefix = "test"
+  }
+  dash_configuration {
+    mpd_location = "test"
+    origin_manifest_type = "MULTI_PERIOD"
+  }
+  name = "test-playback-configuration-awsmt"
+  slate_ad_url = "https://exampleurl.com/"
+  tags = {}
+  transcode_profile_name = "test"
+  video_content_source_url = "https://exampleurl.com/"
 }
 
 output "out" {
-  value = data.awsmt_playback_configuration.c1
+  value = resource.awsmt_playback_configuration.r1
 }
