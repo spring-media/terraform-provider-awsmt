b81568aa5270f9a5e8ed9b73f7ed018173ce6bb6
thatsddr <domenico.diruocco@code.berlin>
2022-02-10 10:59:19 +0100

    chore: add resource schema

--- awsmt/resource_playback_configuration.go
+++ awsmt/resource_playback_configuration.go
@@ -9,7 +9,160 @@ import (
 func resourcePlaybackConfiguration() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourcePlaybackConfigurationCreate,
-		Schema:        map[string]*schema.Schema{},
+		// schema based on: https://docs.aws.amazon.com/mediatailor/latest/apireference/playbackconfiguration.html#playbackconfiguration-prop-putplaybackconfigurationrequest-personalizationthresholdseconds
+		// and https://sourcegraph.com/github.com/aws/aws-sdk-go/-/docs/service/mediatailor#PutPlaybackConfigurationInput
+		Schema: map[string]*schema.Schema{
+			"ad_decision_server_url": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"avail_suppression": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"mode": {
+							// enum: OFF | BEHIND_LIVE_EDGE
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"value": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"bumper": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"end_url": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"start_url": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"cdn_configuration": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ad_segment_url_prefix": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"content_segment_url_prefix": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"configuration_aliases": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type:     schema.TypeMap,
+					Optional: true,
+					Elem: &schema.Schema{
+						Type: schema.TypeString,
+					},
+				},
+			},
+			"dash_configuration": {
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"mpd_location": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"origin_manifest_type": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"live_pre_roll_configuration": {
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ad_decision_server_url": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"max_duration_seconds": {
+							Type:     schema.TypeInt,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"manifest_processing_rules": {
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ad_marker_passthrough": {
+							Type:     schema.TypeList,
+							MaxItems: 1,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"enabled": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"personalization_threshold_seconds": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"slate_ad_url": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"tags": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"transcode_profile_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"video_content_source_url": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
 	}
 }
 
