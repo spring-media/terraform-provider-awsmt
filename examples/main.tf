4aae1521a16bdf814520e413f60478243a4f35b1
thatsddr <domenico.diruocco@code.berlin>
2022-03-09 17:06:26 +0100

    chore: updated example

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     awsmt = {
-      version = "1.4.0"
+      version = "~> 1.7.0"
       source  = "spring-media/awsmt"
       // to use a local version of the provider,
       // run `make` and create a ~/.terraformrc file with the following content:
@@ -15,24 +15,25 @@ terraform {
   }
 }
 
-#data "awsmt_playback_configuration" "c1" {
-#  name="broadcast-staging-live-stream"
-#}
-
-resource "awsmt_playback_configuration" "r1" {
-  ad_decision_server_url = "https://exampleurl.com/"
-  cdn_configuration {
-    ad_segment_url_prefix = "https://exampleurl.com/"
-  }
-  dash_configuration {
-    mpd_location = "DISABLED"
-    origin_manifest_type = "SINGLE_PERIOD"
-  }
-  name = "test-playback-configuration-awsmt"
-  tags = {"Environment": "dev"}
-  video_content_source_url = "https://exampleurl.com/"
+data "awsmt_playback_configuration" "c1" {
+  name = "replay-live-stream"
 }
 
+#resource "awsmt_playback_configuration" "r1" {
+#  ad_decision_server_url = "https://exampleurl.com/"
+#  cdn_configuration {
+#    ad_segment_url_prefix = "https://exampleurl.com/"
+#  }
+#  dash_configuration {
+#    mpd_location = "DISABLED"
+#    origin_manifest_type = "SINGLE_PERIOD"
+#  }
+#  name = "test-playback-configuration-awsmt"
+#  tags = {"Environment": "dev"}
+#  video_content_source_url = "https://exampleurl.com/"
+#}
+
 output "out" {
-  value = resource.awsmt_playback_configuration.r1
+  #  value = resource.awsmt_playback_configuration.r1
+  value = data.awsmt_playback_configuration.c1
 }
