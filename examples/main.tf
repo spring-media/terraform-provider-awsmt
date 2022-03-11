933781a46c24d211eb22057300341163776ce55f
thatsddr <domenico.diruocco@code.berlin>
2022-03-11 10:11:09 +0100

    chore: update examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,39 +1,40 @@
 terraform {
   required_providers {
     awsmt = {
-      version = "~> 1.7.0"
+      version = "~> 1.8.0"
       source  = "spring-media/awsmt"
-      // to use a local version of the provider,
-      // run `make` and create a ~/.terraformrc file with the following content:
-      //provider_installation {
-      //  dev_overrides {
-      //      "spring-media/awsmt" = "/Users/<USERNAME>/.terraform.d/plugins/github.com/spring-media/terraform-provider-awsmt/0.0.1/<SYSTEM_ARCHITECTURE>"
-      //  }
-      //  direct {}
-      //}
     }
   }
 }
 
-data "awsmt_playback_configuration" "c1" {
-  name = "replay-live-stream"
-}
-
-#resource "awsmt_playback_configuration" "r1" {
-#  ad_decision_server_url = "https://exampleurl.com/"
-#  cdn_configuration {
-#    ad_segment_url_prefix = "https://exampleurl.com/"
-#  }
-#  dash_configuration {
-#    mpd_location = "DISABLED"
-#    origin_manifest_type = "SINGLE_PERIOD"
-#  }
-#  name = "test-playback-configuration-awsmt"
-#  tags = {"Environment": "dev"}
-#  video_content_source_url = "https://exampleurl.com/"
+#data "awsmt_playback_configuration" "c1" {
+#  name = "replay-live-stream"
 #}
 
+resource "awsmt_playback_configuration" "r1" {
+  ad_decision_server_url = "https://exampleurl.com/"
+  cdn_configuration {
+    ad_segment_url_prefix = "https://exampleurl.com/"
+  }
+  dash_configuration {
+    mpd_location = "DISABLED"
+    origin_manifest_type = "SINGLE_PERIOD"
+  }
+  live_pre_roll_configuration {
+    max_duration_seconds = 1
+  }
+  manifest_processing_rules {
+    ad_marker_passthrough {
+      enabled = false
+    }
+  }
+  name = "test-playback-configuration-awsmt"
+  personalization_threshold_seconds = 2
+  tags = {"Environment": "dev"}
+  video_content_source_url = "https://exampleurl.com/"
+}
+
 output "out" {
-  #  value = resource.awsmt_playback_configuration.r1
-  value = data.awsmt_playback_configuration.c1
+  value = resource.awsmt_playback_configuration.r1
+  # value = data.awsmt_playback_configuration.c1
 }
