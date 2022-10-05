66ca62f0a55485b1012f689462ef83a9ba725ca7
Olivier Abdoun <olivier.abdoun@axelspringer.com>
2022-10-05 13:11:03 +0200

    test: fix time format check in test
    OTT-2113

--- awsmt/data_source_source_location_test.go
+++ awsmt/data_source_source_location_test.go
@@ -18,7 +18,7 @@ func TestAccSourceLocationDataSourceBasic(t *testing.T) {
 				Config: testAccSourceLocationDataSourceBasic(rName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr(dataSourceName, "arn", regexp.MustCompile(`^arn:aws:mediatailor:[\w-]+:\d+:sourceLocation\/.*$`)),
-					resource.TestMatchResourceAttr(dataSourceName, "creation_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \+\d{4} \w+$`)),
+					resource.TestMatchResourceAttr(dataSourceName, "creation_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d{3})? \+\d{4} \w+$`)),
 					resource.TestMatchResourceAttr(dataSourceName, "last_modified_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \+\d{4} \w+$`)),
 					resource.TestCheckResourceAttr(dataSourceName, "name", rName),
 				),
--- awsmt/data_source_vod_source_test.go
+++ awsmt/data_source_vod_source_test.go
@@ -19,7 +19,7 @@ func TestAccVodSourceDataSourceBasic(t *testing.T) {
 				Config: testAccVodSourceDataSourceBasic(sourceLocationName, vodSourceName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr(dataSourceName, "arn", regexp.MustCompile(`^arn:aws:mediatailor:[\w-]+:\d+:vodSource\/.*$`)),
-					resource.TestMatchResourceAttr(dataSourceName, "creation_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \+\d{4} \w+$`)),
+					resource.TestMatchResourceAttr(dataSourceName, "creation_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d{3})? \+\d{4} \w+$`)),
 					resource.TestMatchResourceAttr(dataSourceName, "last_modified_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \+\d{4} \w+$`)),
 					resource.TestCheckResourceAttr(dataSourceName, "source_location_name", sourceLocationName),
 					resource.TestCheckResourceAttr(dataSourceName, "name", vodSourceName),
