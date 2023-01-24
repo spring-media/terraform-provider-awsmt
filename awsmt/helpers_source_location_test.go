b750e5041d2bb847c1d1bd5b1af55ded3bec71ec
thatsddr <domenico.diruocco@code.berlin>
2023-01-24 13:38:40 +0100

    test: vod and live sources deletion edge case

--- awsmt/helpers_source_location_test.go
+++ awsmt/helpers_source_location_test.go
@@ -6,16 +6,25 @@ import (
 	"testing"
 )
 
-func TestDeleteVodSources(t *testing.T) {
-	// arrange: create source location, add vod sources
+func setUpTestResources(sourceLocationName *string) (*mediatailor.MediaTailor, *mediatailor.HttpPackageConfiguration, error) {
 	conn := testAccProvider.Meta().(*mediatailor.MediaTailor)
-	sourceLocationName := aws.String("source_location_test_vod_deletion")
 	httpConfiguration := &mediatailor.HttpConfiguration{BaseUrl: aws.String("https://www.example.com")}
-	vodSourceName := aws.String("vod_source_test_vod_deletion")
 	if _, err := conn.CreateSourceLocation(&mediatailor.CreateSourceLocationInput{SourceLocationName: sourceLocationName, HttpConfiguration: httpConfiguration}); err != nil {
-		t.Fatalf(`Error creating source location: %v`, err)
+		return nil, nil, err
 	}
 	httpPackageConfiguration := &mediatailor.HttpPackageConfiguration{Path: aws.String("/"), SourceGroup: aws.String("default"), Type: aws.String("HLS")}
+	return conn, httpPackageConfiguration, nil
+}
+
+func TestDeleteVodSources(t *testing.T) {
+	// arrange: create source location, add vod sources
+	sourceLocationName := aws.String("source_location_test_vod_deletion")
+	vodSourceName := aws.String("vod_source_test_vod_deletion")
+	conn, httpPackageConfiguration, err := setUpTestResources(sourceLocationName)
+	if err != nil {
+		t.Fatalf(`Error creating source location: %v`, err)
+	}
+
 	if _, err := conn.CreateVodSource(&mediatailor.CreateVodSourceInput{SourceLocationName: sourceLocationName, VodSourceName: vodSourceName, HttpPackageConfigurations: []*mediatailor.HttpPackageConfiguration{httpPackageConfiguration}}); err != nil {
 		t.Fatalf(`Error creating vod source: %v`, err)
 	}
@@ -33,29 +42,48 @@ func TestDeleteVodSources(t *testing.T) {
 	}
 }
 
+func TestDeleteVodSourcesError(t *testing.T) {
+	// arrange: set up name and connection
+	conn := testAccProvider.Meta().(*mediatailor.MediaTailor)
+	sourceLocationName := aws.String("source_location_test_vod_deletion_error")
+	// act: delete vod sources
+	if err := deleteVodSources(sourceLocationName, conn); err == nil {
+		t.Fatalf(`Source location actually exists`)
+	}
+}
+
 func TestDeleteLiveSources(t *testing.T) {
 	// arrange: create source location, add vod sources
-	conn := testAccProvider.Meta().(*mediatailor.MediaTailor)
 	sourceLocationName := aws.String("source_location_test_vod_deletion")
-	httpConfiguration := &mediatailor.HttpConfiguration{BaseUrl: aws.String("https://www.example.com")}
 	liveSourceName := aws.String("vod_source_test_vod_deletion")
-	if _, err := conn.CreateSourceLocation(&mediatailor.CreateSourceLocationInput{SourceLocationName: sourceLocationName, HttpConfiguration: httpConfiguration}); err != nil {
+	conn, httpPackageConfiguration, err := setUpTestResources(sourceLocationName)
+	if err != nil {
 		t.Fatalf(`Error creating source location: %v`, err)
 	}
-	httpPackageConfiguration := &mediatailor.HttpPackageConfiguration{Path: aws.String("/"), SourceGroup: aws.String("default"), Type: aws.String("HLS")}
+
 	if _, err := conn.CreateLiveSource(&mediatailor.CreateLiveSourceInput{SourceLocationName: sourceLocationName, LiveSourceName: liveSourceName, HttpPackageConfigurations: []*mediatailor.HttpPackageConfiguration{httpPackageConfiguration}}); err != nil {
 		t.Fatalf(`Error creating live source: %v`, err)
 	}
-	// act: delete vod sources
+	// act: delete live sources
 	if err := deleteLiveSources(sourceLocationName, conn); err != nil {
 		t.Fatalf(`Error deleting live sources: %v`, err)
 	}
-	// assert: vod source has been deleted
+	// assert: live source has been deleted
 	if _, err := conn.DescribeLiveSource(&mediatailor.DescribeLiveSourceInput{SourceLocationName: sourceLocationName, LiveSourceName: liveSourceName}); err == nil {
 		t.Fatalf(`LiveSource was not deleted`)
 	}
 	// cleanup
 	if _, err := conn.DeleteSourceLocation(&mediatailor.DeleteSourceLocationInput{SourceLocationName: sourceLocationName}); err != nil {
 		t.Fatalf(`Error cleaning up: %v`, err)
+	}
+}
+
+func TestDeleteLiveSourcesError(t *testing.T) {
+	// arrange: set up name and connection
+	conn := testAccProvider.Meta().(*mediatailor.MediaTailor)
+	sourceLocationName := aws.String("source_location_test_vod_deletion_error")
+	// act: delete live sources
+	if err := deleteLiveSources(sourceLocationName, conn); err == nil {
+		t.Fatalf(`Source location actually exists`)
 	}
 }
