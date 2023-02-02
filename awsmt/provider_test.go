3b84e7098f20d585f423494ca33df2094fddade3
isadora lopes <93722096+isadoral@users.noreply.github.com>
2023-02-02 10:19:00 +0100

    test: implemented test setup

---
+++ awsmt/provider_test.go
@@ -0,0 +1,31 @@
+package awsmt
+
+import (
+	"errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/mediatailor"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"testing"
+)
+
+var (
+	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+		"awsmt": providerserver.NewProtocol6WithError(New()),
+	}
+)
+
+func TestMain(m *testing.M) {
+	resource.TestMain(m)
+}
+
+func sharedClientForRegion(region string) (interface{}, error) {
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+	if err != nil {
+		return nil, errors.New("unable to initialize provider in the specified region")
+	}
+	c := mediatailor.New(sess)
+	return c, nil
+}
