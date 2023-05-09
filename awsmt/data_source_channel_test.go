c6272106d5deb5aa04e0f3656e5217b1234052cc
isadora lopes <93722096+isadoral@users.noreply.github.com>
2023-05-09 14:46:37 +0200

    feat: channel data source tests

--- awsmt/data_source_channel_test.go
+++ awsmt/data_source_channel_test.go
@@ -1,47 +1,50 @@
 package awsmt
 
 import (
-	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"os"
 	"regexp"
 	"testing"
 )
 
+func testAccPreCheck(t *testing.T) {
+	if a, b, c := os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), os.Getenv("AWS_PROFILE"); (a == "" || b == "") && c == "" {
+		t.Fatal("Either AWS_PROFILE or both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be set for acceptance tests")
+	}
+}
+
 func TestAccChannelDataSourceBasic(t *testing.T) {
-	dataSourceName := "data.awsmt_channel.testing"
-	resourceName := "basic_channel"
 	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccChannelDataSourceBasic(resourceName),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestMatchResourceAttr(dataSourceName, "arn", regexp.MustCompile(`^arn:aws:mediatailor:[\w-]+:\d+:channel\/.*$`)),
-					resource.TestMatchResourceAttr(dataSourceName, "creation_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d{1,3})? \+\d{4} \w+$`)),
-					resource.TestMatchResourceAttr(dataSourceName, "last_modified_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d{1,3})? \+\d{4} \w+$`)),
-					resource.TestCheckResourceAttr(dataSourceName, "outputs.0.hls_manifest_windows_seconds", "30"),
-					resource.TestCheckResourceAttr(dataSourceName, "name", resourceName),
-				),
-			},
-		},
-	})
-}
-
-func testAccChannelDataSourceBasic(resourceName string) string {
-	return fmt.Sprintf(`
-resource "awsmt_channel" "testing"  {
-  name = "%[1]s"
+				Config: `
+				resource "awsmt_channel" "test"  {
+  name = "test"
+  channel_state = "STOPPED"
   outputs = [{
     manifest_name                = "default"
     source_group                 = "default"
     hls_playlist_settings = [{manifest_windows_seconds = 30}]
   }]
   playback_mode = "LOOP"
+  policy = "{\"Version\": \"2012-10-17\", \"Statement\": [{\"Sid\": \"AllowAnonymous\", \"Effect\": \"Allow\", \"Principal\": \"*\", \"Action\": \"mediatailor:GetManifest\", \"Resource\": \"arn:aws:mediatailor:eu-central-1:319158032161:channel/test\"}]}"
   tier = "BASIC"
 }
 
-data "awsmt_channel" "read" {
-  name = "sample-1"
+data "awsmt_channel" "test" {
+  name = awsmt_channel.test.name
 }
-`, resourceName)
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestMatchResourceAttr("data.awsmt_channel.test", "arn", regexp.MustCompile(`^arn:aws:mediatailor:[\w-]+:\d+:channel\/.*$`)),
+					resource.TestMatchResourceAttr("data.awsmt_channel.test", "creation_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d{1,3})? \+\d{4} \w+$`)),
+					resource.TestMatchResourceAttr("data.awsmt_channel.test", "last_modified_time", regexp.MustCompile(`^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d{1,3})? \+\d{4} \w+$`)),
+					resource.TestCheckResourceAttr("data.awsmt_channel.test", "name", "test"),
+					resource.TestCheckResourceAttr("data.awsmt_channel.test", "id", "placeholder"),
+				),
+			},
+		},
+	})
 }
