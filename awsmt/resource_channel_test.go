5285cef136c197fd171d2c73f40725af59d8337f
isadora lopes <93722096+isadoral@users.noreply.github.com>
2023-05-16 15:16:00 +0200

    feat: channel resource tests

---
+++ awsmt/resource_channel_test.go
@@ -0,0 +1,50 @@
+package awsmt
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"os"
+	"regexp"
+	"testing"
+)
+
+func testAccPreCheck(t *testing.T) {
+	if a, b, c := os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), os.Getenv("AWS_PROFILE"); (a == "" || b == "") && c == "" {
+		t.Fatal("Either AWS_PROFILE or both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be set for acceptance tests")
+	}
+}
+
+func TestAccChannelResourceBasic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+				resource "awsmt_channel" "resourcetest"  {
+  name = "resourcetest"
+  channel_state = "RUNNING"
+  outputs = [{
+    manifest_name                = "default"
+    source_group                 = "default"
+    hls_playlist_settings = [{manifest_windows_seconds = 30}]
+  }]
+  playback_mode = "LOOP"
+  policy = "{\"Version\": \"2012-10-17\", \"Statement\": [{\"Sid\": \"AllowAnonymous\", \"Effect\": \"Allow\", \"Principal\": \"*\", \"Action\": \"mediatailor:GetManifest\", \"Resource\": \"arn:aws:mediatailor:eu-central-1:319158032161:channel/resourcetest\"}]}"
+  tier = "BASIC"
+}
+data "awsmt_channel" "resourcetest" {
+  name = awsmt_channel.resourcetest.name
+}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestMatchResourceAttr("awsmt_channel.resourcetest", "arn", regexp.MustCompile(`^arn:aws:mediatailor:[\w-]+:\d+:channel\/.*$`)),
+					resource.TestMatchResourceAttr("awsmt_channel.resourcetest", "playback_mode", regexp.MustCompile("LOOP")),
+					resource.TestMatchResourceAttr("awsmt_channel.resourcetest", "tier", regexp.MustCompile("BASIC")),
+					resource.TestCheckResourceAttr("awsmt_channel.resourcetest", "channel_state", "RUNNING"),
+					resource.TestCheckResourceAttr("awsmt_channel.resourcetest", "name", "resourcetest"),
+					resource.TestCheckResourceAttr("awsmt_channel.resourcetest", "outputs.0.manifest_name", "default"),
+				),
+			},
+		},
+	})
+}
