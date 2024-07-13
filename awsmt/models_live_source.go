b24abaf08c90b6a1ede5046f02c4572854d76e81
Domenico Di Ruocco <domenico.di-ruocco@axelspringer.com>
2024-07-13 11:03:42 +0200

    refactor: shared model in separate file OTT-6230

--- awsmt/models_live_source.go
+++ awsmt/models_live_source.go
@@ -12,9 +12,3 @@ type liveSourceModel struct {
 	SourceLocationName        *string                          `tfsdk:"source_location_name"`
 	Tags                      map[string]string                `tfsdk:"tags"`
 }
-
-type httpPackageConfigurationsModel struct {
-	Path        *string `tfsdk:"path"`
-	SourceGroup *string `tfsdk:"source_group"`
-	Type        *string `tfsdk:"type"`
-}
---
+++ awsmt/models_shared.go
@@ -0,0 +1,7 @@
+package awsmt
+
+type httpPackageConfigurationsModel struct {
+	Path        *string `tfsdk:"path"`
+	SourceGroup *string `tfsdk:"source_group"`
+	Type        *string `tfsdk:"type"`
+}
