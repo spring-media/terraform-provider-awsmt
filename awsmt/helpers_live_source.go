c70fa948fcece719aee1073b8ddc5c3fb42376de
Domenico Di Ruocco <domenico.di-ruocco@axelspringer.com>
2024-07-13 10:49:24 +0200

    refactor: remove duplication OTT-6230

--- awsmt/helpers_live_source.go
+++ awsmt/helpers_live_source.go
@@ -2,81 +2,82 @@ package awsmt
 
 import (
 	"github.com/aws/aws-sdk-go-v2/service/mediatailor"
+	awsTypes "github.com/aws/aws-sdk-go-v2/service/mediatailor/types"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-func getLiveSourceInput(plan liveSourceModel) mediatailor.CreateLiveSourceInput {
+func getCreateLiveSourceInput(model liveSourceModel) *mediatailor.CreateLiveSourceInput {
 	var input mediatailor.CreateLiveSourceInput
 
-	if plan.HttpPackageConfigurations != nil && len(plan.HttpPackageConfigurations) > 0 {
-		input.HttpPackageConfigurations = buildHttpPackageConfigurations(plan.HttpPackageConfigurations)
+	input.HttpPackageConfigurations, input.LiveSourceName, input.SourceLocationName = getSharedLiveSourceInput(&model)
+
+	if model.Tags != nil && len(model.Tags) > 0 {
+		input.Tags = model.Tags
 	}
 
-	if plan.Name != nil {
-		input.LiveSourceName = plan.Name
-	}
+	return &input
+}
 
-	if plan.SourceLocationName != nil {
-		input.SourceLocationName = plan.SourceLocationName
-	}
+func getUpdateLiveSourceInput(model liveSourceModel) mediatailor.UpdateLiveSourceInput {
+	var input mediatailor.UpdateLiveSourceInput
 
-	if plan.Tags != nil && len(plan.Tags) > 0 {
-		input.Tags = plan.Tags
-	}
+	input.HttpPackageConfigurations, input.LiveSourceName, input.SourceLocationName = getSharedLiveSourceInput(&model)
 
 	return input
 }
 
-func readLiveSourceToPlan(plan liveSourceModel, liveSource mediatailor.CreateLiveSourceOutput) liveSourceModel {
+func getSharedLiveSourceInput(model *liveSourceModel) ([]awsTypes.HttpPackageConfiguration, *string, *string) {
+	var httpPackageConfigurations []awsTypes.HttpPackageConfiguration
+	var liveSourceName *string
+	var sourceLocationName *string
+
+	if model.HttpPackageConfigurations != nil && len(model.HttpPackageConfigurations) > 0 {
+		httpPackageConfigurations = getHttpPackageConfigurations(model.HttpPackageConfigurations)
+	}
+
+	if model.Name != nil {
+		liveSourceName = model.Name
+	}
+
+	if model.SourceLocationName != nil {
+		sourceLocationName = model.SourceLocationName
+	}
+	return httpPackageConfigurations, liveSourceName, sourceLocationName
+}
+
+// readLiveSource is used for both plan and state since the output from create/update and describe is compatible
+func readLiveSource(model liveSourceModel, liveSource mediatailor.CreateLiveSourceOutput) liveSourceModel {
 	liveSourceName := *liveSource.LiveSourceName
 	sourceLocationName := *liveSource.SourceLocationName
 	idNames := sourceLocationName + "," + liveSourceName
 
-	plan.ID = types.StringValue(idNames)
+	model.ID = types.StringValue(idNames)
 
 	if liveSource.Arn != nil {
-		plan.Arn = types.StringValue(*liveSource.Arn)
+		model.Arn = types.StringValue(*liveSource.Arn)
 	}
 
 	if liveSource.CreationTime != nil {
-		plan.CreationTime = types.StringValue(liveSource.CreationTime.String())
+		model.CreationTime = types.StringValue(liveSource.CreationTime.String())
 	}
 
-	plan.HttpPackageConfigurations = readHttpPackageConfigurations(liveSource.HttpPackageConfigurations)
+	model.HttpPackageConfigurations = readHttpPackageConfigurations(liveSource.HttpPackageConfigurations)
 
 	if liveSource.LastModifiedTime != nil {
-		plan.LastModifiedTime = types.StringValue(liveSource.LastModifiedTime.String())
+		model.LastModifiedTime = types.StringValue(liveSource.LastModifiedTime.String())
 	}
 
 	if liveSource.LiveSourceName != nil {
-		plan.Name = liveSource.LiveSourceName
+		model.Name = liveSource.LiveSourceName
 	}
 
 	if liveSource.SourceLocationName != nil {
-		plan.SourceLocationName = liveSource.SourceLocationName
+		model.SourceLocationName = liveSource.SourceLocationName
 	}
 
 	if len(liveSource.Tags) > 0 {
-		plan.Tags = liveSource.Tags
+		model.Tags = liveSource.Tags
 	}
 
-	return plan
-}
-
-func liveSourceUpdateInput(plan liveSourceModel) mediatailor.UpdateLiveSourceInput {
-	var input mediatailor.UpdateLiveSourceInput
-
-	if plan.HttpPackageConfigurations != nil && len(plan.HttpPackageConfigurations) > 0 {
-		input.HttpPackageConfigurations = buildHttpPackageConfigurations(plan.HttpPackageConfigurations)
-	}
-
-	if plan.Name != nil {
-		input.LiveSourceName = plan.Name
-	}
-
-	if plan.SourceLocationName != nil {
-		input.SourceLocationName = plan.SourceLocationName
-	}
-
-	return input
+	return model
 }
